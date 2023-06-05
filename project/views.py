fb697cadbc03b38bb1792049b1e6848be063ff6d
kawe <kawehenriquedacostarodrigues@gmail.com>
2023-06-05 18:19:00 -0300

    [FEAT] creation of views

--- project/views.py
+++ project/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import ProjectSerializers
+from .models import Project
 
-# Create your views here.
+class RequestTestAPI(APIView):
+
+    def get(self, request, format=None):
+        return Response({"API": "OK"})
+
+class ProjectCreateViewSet(generics.CreateAPIView):
+
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializers
+
+class ProjectRetrieveUpdateDeleteViewSet(generics.RetrieveUpdateDestroyAPIView):
+
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializers
+
+class ProjectListViewSet(generics.ListAPIView):
+
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializers
