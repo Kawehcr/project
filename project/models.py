0b30fbfd50d620aa322d845230d12c4cd7dc4c66
kawe <kawehenriquedacostarodrigues@gmail.com>
2023-06-05 18:00:03 -0300

    [FEAT] creation of models

--- project/models.py
+++ project/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class project(models.Model):
+    id = models.AutoField(primary_key=True)
+    company_id = models.IntegerField(null=False, blank=False, verbose_name="Id da Empresa", db_column="company_id")
+    name_project = models.CharField(max_length=100, verbose_name="Nome do projeto", null=False, blank=False, db_column="name_project")
+    enabled = models.CharField(max_length=3, null=False, blank=False, verbose_name="Ativo", db_column="enabled")
+
+    class Meta:
+        db_table = "project"
